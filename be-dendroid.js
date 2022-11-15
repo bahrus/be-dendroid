30cff965d9e05a65e11ca3617041ae4adcea4eaa
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-11-14 20:29:57 -0500

    Initial commit

--- be-dendroid.js
+++ be-dendroid.js
@@ -1,42 +1,34 @@
-import {define, BeDecoratedProps} from 'be-decorated/DE.js';
-import {Actions, PP, VirtualProps, Proxy, ProxyProps} from './types';
+import { define } from 'be-decorated/DE.js';
 import { register } from 'be-hive/register.js';
-
-export class BeDendroid extends EventTarget implements Actions{
-    async hydrate(pp: PP) {
-        const {buttonsTempl, self} = pp;
-        if(buttonsTempl === undefined){
-            const {defaultTempl} = await import('./defaultTempl.js');
+export class BeDendroid extends EventTarget {
+    async hydrate(pp) {
+        const { buttonsTempl, self } = pp;
+        if (buttonsTempl === undefined) {
+            const { defaultTempl } = await import('./defaultTempl.js');
             self.querySelector('summary')?.appendChild(defaultTempl.content.cloneNode(true));
         }
-        
     }
 }
-
 const tagName = 'be-dendroid';
 const ifWantsToBe = 'dendroid';
 const upgrade = 'details';
-
-define<Proxy & BeDecoratedProps<VirtualProps, Actions>, Actions>({
-    config:{
+define({
+    config: {
         tagName,
-        propDefaults:{
+        propDefaults: {
             ifWantsToBe,
             upgrade,
             virtualProps: ['buttonsTempl', 'hydratingTransform'],
             proxyPropDefaults: {
-                hydratingTransform: {
-
-                }
+                hydratingTransform: {}
             }
         },
         actions: {
             hydrate: 'hydratingTransform'
         }
     },
-    complexPropDefaults:{
+    complexPropDefaults: {
         controller: BeDendroid
     }
 });
-
 register(ifWantsToBe, upgrade, tagName);
